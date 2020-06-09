6ff676ae951b24e27d1f4301d2c2e0158d890cd5
mdtanveeralamgir <mdtanveralamgir.com>
2020-06-09 04:47:51 -0400

    Completed the landing page

---
+++ GUI/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Warm up project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+    <div class="container">
+        <!-- Introduction -->
+        <section class="text-center mt-5 bg-light">
+            <h3> Concordia University </h3>
+            <h3>Dept. of Computer Science & Software Engineering</h3>
+            <h3>Comp 5531- Files And Databases</h3>
+            <p class="display-4">Warm-Up project</p>
+            <h3>Team</h3>
+            <div class="d-flex justify-content-center">
+                <div class="row">
+                    <div class="col-sm">
+                        <ul class="list-group bg-light">
+                            <li class="list-group-item bg-light border-0">Osman</li>
+                            <li class="list-group-item bg-light border-0">Craig</li>
+                        </ul>
+                    </div>
+                    <div class="col-sm bg-light">
+                        <ul class="list-group">
+                            <li class="list-group-item bg-light border-0">Fan</li>
+                            <li class="list-group-item bg-light border-0">Tanveer</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- E/R diagram and All tables -->
+        <section class="mt-3 p-2 bg-light">
+            <p class="font-weight-bolder">Link to E/R Diagram (Lucid chart) and All tables in the DB</p>
+            <div class="erDiagram mt-2 d-flex justify-content-between">
+                <a href="https://app.lucidchart.com/invitations/accept/2fe3aa92-88d3-4b64-9048-ec2878a1bb2b" target="_blank" class="btn btn-secondary stretch-link">E/R Diagram</a>
+                <a href="#" class="btn btn-secondary stretch-link">All tables</a>   
+            </div>
+        </section>
+        <!-- Queries -->
+        <section class="bg-light mt-3 p-2">
+            <p class="font-weight-bolder">Click each link below to run the query</p>
+            <div class="queries mt-3">
+                <div class="row">
+                    <div class="col-md">
+                        <ol class="list-group">
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">i. Get details of all books in stock.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">ii. Get details of all back orders.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">iii. For a given customer, get details of all his/her back orders.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">iv. For a given customer, get details of all his/her purchases made during a
+                            specific period of time.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">v. Give a report of sales during a specific period of time.</a></li>
+                        </ol>
+                    </div>
+                    <div class="col-md">
+                        <ol class="list-group">
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">vi. Find the title of book(s) that have the highest back-order.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">vii. Give details of books that are supplied by a given publisher.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">viii. For a given publisher, get details of the head office and all the branches for that publisher.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">ix. Get details of books that are in the inventory but there have never been a purchase for that specific book.</a></li>
+                            <li class="list-group-item bg-light border-0"><a href="#" class="btn btn-secondary stretch-link">x. Get details of all books that are in the inventory for a given author..</a></li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
